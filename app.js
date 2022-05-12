61319ee8a1104363a4cedff65387ab9acf2f78d0
Macy Kay Mannix <macymannix@gmail.com>
2022-05-11 23:12:22 -0700

    modularize interpole

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-const profileDataArgs = process.argv.slice(2);
 
-const printProfileData = profileDataArr => {
-  // This...
-  for (let i = 0; i < profileDataArr.length; i += 1) {
-    console.log(profileDataArr[i]);
-  }
+const fs = require('fs');
 
-  console.log('================');
+const generatePage = require('./src/page-template.js');
 
-  // Is the same as this...
-  profileDataArr.forEach(profileItem => console.log(profileItem));
-};
+const profileDataArgs = process.argv.slice(2, process.argv.length);
 
-printProfileData(profileDataArgs);
+const [name, github] = profileDataArgs;
+
+
+fs.writeFile('index.html', generatePage(name, github), err => {
+  if (err) throw err;
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
