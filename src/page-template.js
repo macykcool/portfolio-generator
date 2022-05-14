34ceea20ee177b60757b659132bfe6463b49a620
Macy Kay Mannix <macymannix@gmail.com>
2022-05-13 23:55:47 -0700

    9 5 4

--- src/page-template.js
+++ src/page-template.js
@@ -54,80 +54,8 @@ const generateProjects = projectsArr => {
   `;
 };
 
-//   const featuredProjects = projectsArr.filter(project => {
-//     if (project.feature) {
-//       return true;
-//     } else {
-//       return false;
-//     }
-//   });
-
-//   const nonFeaturedProjects = projectsArr.filter(project => {
-//     if (!project.feature) {
-//       return true;
-//     } else {
-//       return false;
-//     }
-//   });
-//   const featuredProjectHtmlArr = featuredProjects.map(({ name, description, languages, link }) => {
-//     return `
-//       <div class="col-12 mb-2 bg-dark text-light p-3 flex-column">
-//         <h3 class="portfolio-item-title text-light">${name}</h3>
-//         <h5 class="portfolio-languages">
-//           Built With:
-//           ${languages.join(', ')}
-//         </h5>
-//         <p>${description}</p>
-//         <a href="${link}" class="btn mt-auto"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
-//       </div>
-//     `;
-//   });
-
-//   const nonFeaturedProjectHtmlArr = nonFeaturedProjects.map(
-//     ({ name, description, languages, link }) => {
-//       return `
-//         <div class="col-12 col-md-6 mb-2 bg-dark text-light p-3 flex-column">
-//           <h3 class="portfolio-item-title text-light">${name}</h3>
-//           <h5 class="portfolio-languages">
-//             Built With:
-//             ${languages.join(', ')}
-//           </h5>
-//           <p>${description}</p>
-//           <a href="${link}" class="btn mt-auto"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
-//         </div>
-//       `;
-//     }
-//   );
-
-
-//   const projectHtmlArr = projectsArr.map(({ name, description, languages, link }) => {
-//     return `
-//         <div class="col-12 col-md-6 mb-2 bg-dark text-light p-3 flex-column">
-//           <h3 class="portfolio-item-title text-light">${name}</h3>
-//           <h5 class="portfolio-languages">
-//             Built With:
-//             ${languages.join(', ')}
-//           </h5>
-//           <p>${description}</p>
-//           <a href="${link}" class="btn mt-auto"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
-//         </div>
-//       `;
-//   });
-//   return `
-//     <section class="my-3" id="portfolio">
-//       <h2 class="text-dark bg-primary p-2 display-inline-block">Work</h2>
-//       <div class="flex-row justify-space-between">
-//       ${featuredProjectHtmlArr.join('')}
-//       ${nonFeaturedProjectHtmlArr.join('')}
-//       </div>
-//     </section>
-//   `;
-// };
-
-
-
 module.exports = templateData => {
-  // console.log(templateData);
+ 
 const { projects, about, ...header } =templateData
   return `
   <!DOCTYPE html>
